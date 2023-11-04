3ca0e662fc9b593679d021edc21087176cf6e162
VikVanDik <95vlc17@gmail.com>
2023-11-04 16:37:21 +0100

    [add] : control length

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php 
+<?php
+
+$min = 8;
+$max = 32;
+$message = "Scegliere una password con un minimo di $min caratteri ed un massimo di $max caratteri";
+
+if (isset($_POST['characters']) && !empty($_POST['characters'])) {
+    $passlen = $_POST['characters'];
+    if ($passlen < $min || $passlen > $max) {
+        $message = "Errore, scegliere un numero tra $min e $max";
+    }
+}
 
 ?>
 
@@ -15,21 +26,20 @@
     <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <div class="container my-5 ">
+    <div class="container my-5">
         <h1 class="text-center">Strong Password Generator</h1>
         <h2 class="text-center">Genera una password sicura</h2>
         <div class="container">
             <div class="message d-flex align-items-center justify-content-center">
-                <p>Scegliere una password con un minimo di 8 caratteri ed un massimo di 32 caratteri.</p>
+                <p><?php echo $message ?></p>
             </div>
             <div class="form">
-                <form action="index.php">
+                <form action="index.php" method="POST">
                     <label for="characters" class="label">Lunghezza password:</label>
                     <input type="number" id="characters" name="characters" class="number"/>
                     <div class="buttons">
                         <button type="submit" class="btn btn-success">Invia</button>
                         <button type="submit" class="btn btn-danger">Annulla</button>
-
                     </div>
                 </form>
             </div>
